9189b387e40b01ea5b32ec5b85066f995e8520fd
Roqueheli <roqueheliarenas@gmail.com>
2024-06-28 11:38:30 -0400

    add Dockerfile

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [characaters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://rickandmortyapi.com/api/character/")
+      .then(rs => setCharacters(rs.data.results))
+      .catch(err => console.log(err))
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Personajes de Rick and Morty</h1>
+      {characaters?.map(characater => (
+        <div key={characater.id}>
+          <h2>{characater.name}</h2>
+          <p>Especie: {characater.species}</p>
+          <img src={characater.image} alt={characater.name} />
+        </div>
+      ))}
     </div>
   );
 }
